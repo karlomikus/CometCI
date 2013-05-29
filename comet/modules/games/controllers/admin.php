7d72c2f4737875eb1ae7ef92930f5953d12a66c0
Karlo Mikus <eggzy12@gmail.com>
2013-05-29 20:37:28 +0200

    Games modules tested and finished

--- comet/modules/games/controllers/admin.php
+++ comet/modules/games/controllers/admin.php
@@ -2,14 +2,17 @@
 
 class Admin extends Backend_Controller {
 
-    private $assets_path = './assets/games/';
+    private $folder_path = './assets/games/';
 
-    public function __construct() {
+    public function __construct()
+    {
         parent::__construct();
+
         $this->load->model('games_m');
     }
 
-    public function index() {
+    public function index()
+    {
         $this->games_m->order_by('name');
 
         $this->template
@@ -18,45 +21,47 @@ class Admin extends Backend_Controller {
             ->build('admin/main');
     }
 
-    public function create() {
-
+    public function create()
+    {
         $this->load->helper('form');
         $this->load->library('form_validation');
 
         $this->form_validation->set_rules('name', 'Game name', 'required');
-        $this->form_validation->set_rules('shortcode', 'Shortcode', 'required|callback_check_shortcode');
-        $this->form_validation->set_rules('icon', 'Icon', 'required');
+        $this->form_validation->set_rules('shortcode', 'Shortcode', 'required|max_length[4]|is_unique[games.shortcode]');
 
-        $config['upload_path']   = $this->assets_path;
+        $config['upload_path']   = $this->folder_path;
         $config['allowed_types'] = 'gif|jpg|png';
-        $config['max_size']      = '1024';
-        $config['max_width']     = '32';
-        $config['max_height']    = '32';
+        $config['max_size']      = '2024';
+        $config['max_width']     = '128';
+        $config['max_height']    = '128';
         
-        if ($this->form_validation->run() == TRUE) {
-
+        if ($this->form_validation->run() == TRUE)
+        {
             $shortcode = strtolower($this->input->post('shortcode'));
             $config['file_name'] = $shortcode;
             $this->load->library('upload', $config);
 
-            if ($this->upload->do_upload('icon')) {
+            if ($this->upload->do_upload('icon'))
+            {
                 $icon_data = $this->upload->data();
             }
-            else {
-                $this->session->set_flashdata('create_error', "There was an error with the file!");
-                $icon_data = NULL;
+            else
+            {
+                $this->session->set_flashdata('file_error', $this->upload->display_errors('', ''));
+                redirect('admin/games/create');
             }
 
             $data = array(
                 'name' => $this->input->post('name'),
                 'shortcode' => $shortcode,
                 'icon' => $icon_data['file_name']
-            );    
+            );
 
             $this->games_m->insert($data);
             redirect('admin/games');
         }
-        else {
+        else
+        {
             $this->template
             ->set('title', 'Create Game')
             ->build('admin/form');
@@ -69,13 +74,13 @@ class Admin extends Backend_Controller {
         $this->load->library('form_validation');
 
         $this->form_validation->set_rules('name', 'Game name', 'required');
-        $this->form_validation->set_rules('shortcode', 'Shortcode', 'required');
+        $this->form_validation->set_rules('shortcode', 'Shortcode', 'required|max_length[4]');
 
-        $config['upload_path']   = $this->assets_path;
+        $config['upload_path']   = $this->folder_path;
         $config['allowed_types'] = 'gif|jpg|png';
-        $config['max_size']      = '1024';
-        $config['max_width']     = '32';
-        $config['max_height']    = '32';
+        $config['max_size']      = '2024';
+        $config['max_width']     = '128';
+        $config['max_height']    = '128';
 
         if ($this->form_validation->run() == TRUE) 
         {
@@ -83,20 +88,31 @@ class Admin extends Backend_Controller {
             $config['file_name'] = $shortcode;
             $this->load->library('upload', $config);
 
+            if(!empty($_FILES['banner']['icon']))
+            {
+                if ($this->upload->do_upload('icon'))
+                {
+                    $icon_data = $this->upload->data();
+                }
+                else
+                {
+                    $this->session->set_flashdata('file_error', $this->upload->display_errors('', ''));
+                    redirect('admin/games/create');
+                }
+            }
+
+            $fileIcon = $this->games_m->get($id)->icon;
+            if(!empty($icon_data))
+            {
+                unlink($this->folder_path.$fileIcon);
+                $fileIcon = $icon_data['file_name'];
+            }
+
             $data = array(
                 'name' => $this->input->post('name'),
-                'shortcode' => $shortcode
+                'shortcode' => $shortcode,
+                'icon' => $fileIcon
             );
-
-            if (isset($_FILES['icon']) && $this->upload->do_upload('icon')) 
-            {
-                $icon_data = $this->upload->data();
-                $data['icon'] = $icon_data['file_name'];
-            }
-            else 
-            {
-                $this->session->set_flashdata('create_error', "There was an error with the file!");
-            }
 
             $this->games_m->update($id, $data);
             redirect('admin/games');
@@ -110,32 +126,15 @@ class Admin extends Backend_Controller {
         }
     }
 
-    public function delete($id = 0) {
+    public function delete($id = 0)
+    {
         $this->game = $this->games_m->get($id);
-        $filepath = $this->assets_path.$this->game->icon;
+        $filepath = $this->folder_path.$this->game->icon;
+
         unlink($filepath);
         $this->games_m->delete($id);
+
         redirect('admin/games');
     }
 
-    /* Check if shortcode is already in the database */
-    public function check_shortcode($shortcode) {
-        $games_array = $this->games_m->as_array()->get_all();
-        if ($this->_recursive_array_search($shortcode, $games_array)) {
-            $this->form_validation->set_message('check_shortcode', 'This shortcode is already in the database');
-            return FALSE;
-        }
-        return TRUE;
-    }
-
-    /* Search multidimensional array for $needle */
-    private function _recursive_array_search($needle,$haystack) {
-        foreach($haystack as $key=>$value) {
-            $current_key=$key;
-            if($needle===$value OR (is_array($value) && $this->_recursive_array_search($needle,$value) !== false)) {
-                return $current_key;
-            }
-        }
-        return false;
-    }
 }
--- comet/modules/games/views/admin/form.php
+++ comet/modules/games/views/admin/form.php
@@ -1,3 +1,10 @@
+<?php if($this->session->flashdata('file_error')) : ?>
+<div class="alert alert-error">
+	<button type="button" class="close" data-dismiss="alert">&times;</button>
+	<?php echo $this->session->flashdata('file_error'); ?>
+</div>
+<?php endif; ?>
+
 <?php echo validation_errors(); ?>
 <?php echo form_open_multipart(uri_string(), array('class' => 'cms-form')); ?>
 
@@ -6,15 +13,16 @@
 	<input placeholder="Shortcode" class="input-medium" type="text" name="shortcode" value="<?php echo isset($data) ? $data['shortcode'] : set_value("shortcode"); ?>" />
 
 	<div class="cms-upload">
-		<?php if(isset($data['icon'])): ?>
-			<img src="<?php echo base_url(); ?>/assets/admin/games/<?php echo $data['icon']; ?>" />
+		<?php if(isset($data) && !empty($data['icon'])): ?>
+			<img src="<?php echo base_url(); ?>/assets/games/<?php echo $data['icon']; ?>" />
 			<hr />
 		<?php endif; ?>
-		<p>Choose game icon</p> <a class="btn btn-cms-orange show-file-input" href="#"><i class="icon-cloud-upload"></i></a>
+		<p>Choose game icon <strong>(max. 128x128 px)</strong></p> <a class="btn btn-cms-orange show-file-input" href="#file-icon"><i class="icon-cloud-upload"></i></a>
 	</div>
 
 	<button type="submit" class="btn btn-large btn-cms-orange">Save post</button>
 	<a class="btn btn-large btn-cms" href="<?php echo base_url(); ?>admin/<?php echo $this->uri->segment(2); ?>">Cancel</a>
-	<input class="hidden" id="file-input" type="file" name="icon" size="20" />
+
+	<input class="hidden" id="file-icon" type="file" name="icon" size="20" />
 
 <?php echo form_close(); ?>
