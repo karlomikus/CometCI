3a38d8f9d83d5855a327417e2901a3ded0b482aa
Karlo Mikus <eggzy12@gmail.com>
2013-06-24 12:44:37 +0200

    Bugfix: Errors shown when no records found

--- comet/widgets/wi_upcoming/controllers/wi_upcoming.php
+++ comet/widgets/wi_upcoming/controllers/wi_upcoming.php
@@ -11,9 +11,13 @@ class Wi_upcoming extends Frontend_Controller {
 		$this->matches_m->limit(1);
 		$query = $this->matches_m->get_upcoming_matches();
 
-		$match->opponentlogo = get_logo($query[0]->opponent, 'opponent');
-		$match->teamlogo = get_logo($query[0]->team, 'team');
-		$match->date = $query[0]->date;
+		$match = NULL;
+		if(!empty($query))
+		{
+			$match->opponentlogo = get_logo($query[0]->opponent, 'opponent');
+			$match->teamlogo = get_logo($query[0]->team, 'team');
+			$match->date = $query[0]->date;
+		}
 
 		echo $this->template
 			->set('match', $match)
