814bbe464bd4ecdb02fa9f0d58e0bd94061649a8
Karlo Mikus <eggzy12@gmail.com>
2013-07-12 13:27:37 +0200

    Started work on slider module and widget

---
+++ comet/modules/slider/controllers/admin.php
@@ -0,0 +1,141 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Admin extends Backend_Controller {
+
+	private $folder_path = './uploads/slider/';
+
+	public function __construct()
+	{
+		parent::__construct();
+
+		$this->load->model('labels_m');
+	}
+
+	public function index()
+	{
+		$this->load->helper('text');
+		$this->labels_m->order_by('name', 'asc');
+
+		$this->template
+			->set('title', 'Labels')
+			->set('labels', $this->labels_m->get_all())
+			->build('admin/main');
+	}
+
+	public function create()
+	{
+		$this->load->helper('form');
+		$this->load->library('form_validation');
+		$this->load->library('upload');
+
+		$this->form_validation->set_rules('title', 'Title', 'required|htmlspecialchars|trim|min_length[4]|xss_clean');
+
+		if ($this->form_validation->run() == TRUE)
+		{
+			$next_id = $this->labels_m->get_next_id();
+
+			if (!empty($_FILES['banner']['name']))
+			{
+				$config['upload_path']   = $this->folder_path;
+				$config['allowed_types'] = 'gif|jpg|png';
+				$config['max_size']      = '0';
+				$config['max_width']     = '1000';
+				$config['max_height']    = '1000';
+				$config['file_name']     = $next_id;
+				$this->upload->initialize($config);
+
+				if ($this->upload->do_upload('banner'))
+				{
+					$file_data = $this->upload->data();
+				}
+				else
+				{
+					$this->session->set_flashdata('create_error', $this->upload->display_errors('', ''));
+                	$file_data = NULL;
+				}
+			}
+
+			$data = array(
+				'name' => $this->input->post('title'),
+				'description' => $this->input->post('description', TRUE),
+				'banner' => $file_data['file_name']
+			);
+
+			$this->labels_m->insert($data);
+			redirect('admin/labels');
+		}
+		else
+		{
+			$this->template
+				->set('title', 'Create label')
+				->build('admin/form');
+		}
+	}
+
+	public function edit($id = 0)
+	{
+		$this->load->helper('form');
+		$this->load->library('form_validation');
+		$this->load->library('upload');
+
+		$this->form_validation->set_rules('title', 'Title', 'required|min_length[4]|xss_clean');
+
+		if ($this->form_validation->run() == TRUE)
+		{
+			if (!empty($_FILES['banner']['name']))
+			{
+				$config['upload_path']   = $this->folder_path;
+				$config['allowed_types'] = 'gif|jpg|png';
+				$config['max_size']      = '0';
+				$config['max_width']     = '1000';
+				$config['max_height']    = '1000';
+				$config['file_name']     = $id;
+				$this->upload->initialize($config);
+
+				if ($this->upload->do_upload('banner'))
+				{
+					$file_data = $this->upload->data();
+				}
+				else {
+					$this->session->set_flashdata('create_error', $this->upload->display_errors('', ''));
+                	$file_data = NULL;
+				}
+			}
+
+			// Found new file delete the old one
+			$fileBanner = $this->labels_m->get($id)->banner;
+			if(!empty($file_data))
+			{
+				unlink($this->folder_path.$fileBanner);
+				$fileBanner = $file_data['file_name'];
+			}
+			
+			$data = array(
+				'name' => $this->input->post('title'),
+				'description' => $this->input->post('description', TRUE),
+				'banner' => $fileBanner
+			);
+
+			$this->labels_m->update($id, $data);
+			redirect('admin/labels');
+		}
+		else
+		{
+			$this->template
+				->set('title', 'Edit label')
+				->set('data', $this->labels_m->as_array()->get($id))
+				->build('admin/form');
+		}
+	}
+
+	public function delete($id = 0)
+	{
+		$label = $this->labels_m->get($id);
+		$filepath_banner = $this->folder_path.$label->banner;
+
+		unlink($filepath_banner);
+		$this->labels_m->delete($id);
+
+		redirect('admin/labels');
+	}
+}
---
+++ comet/modules/slider/views/admin/main.php
@@ -0,0 +1,38 @@
+<?php if($this->session->flashdata('create_error')) : ?>
+<div class="alert alert-error">
+	<button type="button" class="close" data-dismiss="alert">&times;</button>
+	<?php echo $this->session->flashdata('create_error'); ?>
+</div>
+<?php endif; ?>
+
+<table class="table tbl-custom">
+	<thead>
+		<tr>
+			<th>Name</th>
+			<th>Description</th>
+			<th class="action-add"><a href="<?php echo current_url().'/create';?>"><img src="<?php echo base_url(); ?>assets/admin/img/icon-add.gif" alt="Add" /></a></th>
+		</tr>
+	</thead>
+	<tbody>
+		<?php if(!empty($labels)): ?>
+		<?php foreach ($labels as $label): ?>
+		<tr>
+			<td><?php echo ellipsize($label->name, 20); ?></td>
+			<td><?php echo ellipsize($label->description, 120); ?></td>
+			<td class="action">
+				<a class="action-icon" href="#">Action</a>
+				<ul class="action-list" style="display: none;">
+					<li><a class="confirm-delete" href="<?php echo site_url('admin/labels/delete/'.$label->id); ?>"><i class="icon-trash icon-large"></i></a></li>
+					<li><a href="<?php echo site_url('admin/labels/edit/'.$label->id); ?>"><i class="icon-edit icon-large"></i></a></li>
+				</ul>
+			</td>
+		</tr>
+		<?php endforeach;?>
+		<?php else: ?>
+		<tr>
+			<td colspan="2" class="text-center">There are no labels to show. Create a new one!</td>
+			<td class="text-right"><i class="icon-arrow-up"></i></td>
+		</tr>
+		<?php endif; ?>
+	</tbody>
+</table>
