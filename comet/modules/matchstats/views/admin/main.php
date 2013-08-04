ae3855590b329a45a33c70a7e99c6db73854add4
kmikus <kmikus@localhost>
2013-08-04 17:32:00 +0200

    Match statistics module update, mostly placeholders for now

--- comet/modules/matchstats/views/admin/main.php
+++ comet/modules/matchstats/views/admin/main.php
@@ -1,3 +1,5 @@
+<script src="<?php echo base_url(); ?>assets/admin/js/raphael-min.js"></script>
+<script src="http://cdn.oesmith.co.uk/morris-0.4.3.min.js"></script>
 <?php 
 $winRate = ($data->totalWins / $data->totalMatches) * 100;
 $drawRate = ($data->totalDraws / $data->totalMatches) * 100;
@@ -32,11 +34,97 @@ $loseRate = ($data->totalLoses / $data->totalMatches) * 100;
 			<div class="progress">
 				<div class="bar bar-danger" style="width: <?php echo $loseRate; ?>%;"></div>
 			</div>
+
+			<br />
+
+			<div class="stats-box">
+				<h2><?php echo number_format($winRate, 1); ?>%</h2>
+				<small>Win rate</small>
+			</div>
+
+			<div class="stats-box">
+				<h2>WIN</h2>
+				<small>Last match result</small>
+			</div>
+
+			<div class="stats-box">
+				<h2>3</h2>
+				<small>Matches per day</small>
+			</div>
 		</div>
 	</div>
 
 	<div class="span8 cms-box">
-		Nice graph todo...
+		<div class="header-white">
+			<h4 class="header-thin">Year <?php echo date('Y') ?> progress</h4>
+		</div>
+		<div id="year-graph"></div>
 	</div>
 
 </div>
+
+<div class="row-fluid">
+	
+	<div class="span4 cms-box">
+		<div class="header-white">
+			<h4 class="header-thin">Top 5 Active Players</h4>
+		</div>
+
+		<div class="cms-box-content">
+
+			<div class="media">
+				<a class="pull-left" href="#">
+					<img class="media-object" src="<?php echo base_url(); ?>uploads/users/noavatar.jpg" alt="avatar" style="height: 50px;" />
+				</a>
+				<div class="media-body">
+					<h5 class="media-heading">administrator</h5>
+					Played in 15 games
+				</div>
+			</div>
+
+		</div>
+	</div>
+
+	<div class="span4 cms-box">
+		<div class="header-white">
+			<h4 class="header-thin">Top 5 Best Games</h4>
+		</div>
+	</div>
+
+	<div class="span4 cms-box">
+		<div class="header-white">
+			<h4 class="header-thin">Top 5 Squads</h4>
+		</div>
+	</div>
+
+</div>
+
+<script>
+$(document).ready(function() {
+// Visits graph -----------------------------------
+	new Morris.Area({
+		element: 'year-graph',
+		data: [
+			{ month: '2013-01', wins: 20, draws: 4, loses: 3 },
+			{ month: '2013-02', wins: 10, draws: 5, loses: 6 },
+			{ month: '2013-03', wins: 30, draws: 4, loses: 3 },
+			{ month: '2013-04', wins: 20, draws: 7, loses: 8 },
+			{ month: '2013-05', wins: 10, draws: 3, loses: 2 },
+			{ month: '2013-06', wins: 5, draws: 8, loses: 9 },
+			{ month: '2013-07', wins: 20, draws: 1, loses: 3 },
+			{ month: '2013-08', wins: 10, draws: 1, loses: 6 },
+			{ month: '2013-09', wins: 30, draws: 4, loses: 3 },
+			{ month: '2013-10', wins: 20, draws: 7, loses: 8 },
+			{ month: '2013-11', wins: 10, draws: 9, loses: 2 },
+			{ month: '2013-12', wins: 5, draws: 9, loses: 9 }
+		],
+		lineColors: ['#22c774', '#ff8400', '#c64747'],
+		fillOpacity: 0.2,
+		smooth: true,
+		xkey: 'month',
+		ykeys: ['wins', 'draws', 'loses'],
+		labels: ['Wins', 'Draws', 'Loses'],
+		behaveLikeLine: true
+	});
+});
+</script>
